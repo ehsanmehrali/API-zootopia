44721ceece8ef591156244dabe47823db052e211
ehsanmehrali428@gmail.com <ehsanmehrali428@gmail.com>
2025-04-18 18:11:10 +0200

    Feature of displaying selected animal information added

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,5 +5,30 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
-print(animals_data)
+
+def print_animals_info(animals):
+    """ Prints selected information for each animal. """
+    for animal in animals:
+        print(f"name: {animal.get('name', 'Unknown')}")
+
+        # Locations
+        locations = animal.get("locations", [])
+        print("locations:", " ".join(locations))
+
+        # Characteristics
+        characteristics = animal.get("characteristics", {})
+        for key in ["diet", "type"]:
+            if key in characteristics:
+                print(f"{key}: {characteristics[key]}")
+
+        print("...........")
+
+
+def main():
+    """ Runs main services such as display animals infos """
+    animals_data = load_data('animals_data.json')
+    print_animals_info(animals_data)
+
+
+if __name__ == "__main__":
+    main()
